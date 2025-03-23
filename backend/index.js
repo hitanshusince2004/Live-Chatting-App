34599b382e80e503e779ac7031e571674de8a715
hitanshusince2004 <hitanshu.k@somaiya.edu>
2025-03-23 15:36:25 +0530

    Touch up

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,10 @@ import express from "express";  // ✅ Now properly imported
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
-import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import { connectDB } from "./src/lib/db.js";
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
+import { app, server } from "./src/lib/socket.js";
 
 const PORT = process.env.PORT || 5001; // Default to 5001 if env variable is missing
 
